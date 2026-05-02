b216dfe562d81db9967df5f4c1aa5485c56dcf72
NANA-MARC <nanamarc5547@gmail.com>
2026-05-02 11:35:41 +0000

    Add .gitignore: exclude files/ and mobileblock/ directories

--- mobileblock/lib/app/modules/public_verify/controllers/public_verify_controller.dart
+++
@@ -1,5 +0,0 @@
-import 'package:get/get.dart';
-
-class PublicVerifyController extends GetxController {
-  // Logic for public verification
-}
--- mobileblock/lib/app/modules/result/controllers/result_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-
-class ResultController extends GetxController {
-  final isSuccess = true.obs;
-
-  void toggleResult() => isSuccess.value = !isSuccess.value;
-}
